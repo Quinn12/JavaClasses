f3cf1705ea59245171e948e2996e217bd27d27db
Quinn12 <qhastings@students.mchenry.edu>
2018-10-05 14:58:53 -0500

    Add files via upload

---
+++ src/com/company/utilities/Information.java
@@ -0,0 +1,15 @@
+package com.company.utilities;
+
+public class Information
+{
+    public static void main(String EmpName, int EmpNum, int Shift)
+    {
+
+        System.out.println("#Employee#");
+        System.out.println("Shift: " + Shift);
+        System.out.println("Name: " + EmpName);
+        System.out.println("Id: " + EmpNum);
+        System.out.println("\n");
+
+    }
+}
